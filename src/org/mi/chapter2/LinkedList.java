6119085b0c25da884599e16eba2186e7da5da935
Rosesg <mizhe2016@163.com>
2017-07-13 21:41:59 +0800

    第二章：Java数据结构之链表

---
+++ src/org/mi/chapter2/LinkedList.java
@@ -0,0 +1,54 @@
+package org.mi.chapter2;
+
+/**
+ *  链表
+ */
+public class LinkedList {
+    private Node root;
+
+    // 暴露给外部的添加方法
+    public void add(int data) {
+        if (root == null) {
+            this.root = new Node(data);
+        }else {
+            this.root.addNode(data);
+        }
+    }
+    // 暴露给外部的打印方法
+    public void print() {
+        if (root != null) {
+            System.out.println(root.getData() + "、");
+            this.root.printNode();
+        }
+    }
+
+    private class Node {
+        private int data;
+        private Node next;
+
+        private Node(int data) {
+            this.data = data;
+        }
+
+        private int getData() {
+            return data;
+        }
+
+        // 添加节点
+        private void addNode(int data) {
+            if (this.next == null) {  // 出口
+                this.next = new Node(data);
+            }else {
+                this.next.addNode(data); // 递归
+            }
+        }
+
+        // 打印节点
+        private void printNode() {
+            if (this.next != null) {    // 出口
+                System.out.println(this.next.getData() + "、");
+                this.next.printNode(); // 递归
+            }
+        }
+    }
+}
---
+++ src/org/mi/chapter2/LinkedListTest.java
@@ -0,0 +1,17 @@
+package org.mi.chapter2;
+
+/**
+ * 测试链表类
+ */
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(4);
+        linkedList.add(1);
+        linkedList.add(3);
+        linkedList.add(5);
+        linkedList.add(6);
+        linkedList.add(9);
+        linkedList.print();
+    }
+}
